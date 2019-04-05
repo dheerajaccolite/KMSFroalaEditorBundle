a50a2934f8a1580d4af9eb345fb93d7a5a012d2a
Parashurm <parashuram.dnyanobavharagar@accoliteindia.com>
2019-04-05 10:08:08 +0530

    Update assets and version

--- Utility/UConfiguration.php
+++ Utility/UConfiguration.php
@@ -165,7 +165,7 @@
 
 		public static $OPTIONS_STRING_CUSTOM = array(
 			"customJS"          => null, //
-			"basePath"          => "/bundles/kmsfroalaeditor/froala_editor_3.0.0", //
+			"basePath"          => "/bundles/kmsfroalaeditor/froala_editor_3.0.0-beta.1", //
 			"imageUploadFolder" => "/upload", //
 			"imageUploadPath"   => null, //
 			"fileUploadFolder"  => "/upload", //
